3bc150930249f2115430089bc7460e7ec3a95b15
Dinesh Pabbi <dineshpabbi10@gmail.com>
2021-10-17 14:32:16 -0700

    add basic auth

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 import requests
 import os
 
@@ -15,10 +16,17 @@ port_app = int(os.environ.get('PORT', 33507))
 
 @app.route('/sendAction')
 def hello_world():
-    response_out = requests.post('https://api.github.com/repos/dineshpabbi10/github-action-deals/dispatches', headers=headers, data=data)
-    return jsonify(
-        message="Action Sent"
-    )
+    password = request.args.get('password')
+    if(password == os.environ['PASSWORD']):
+        response_out = requests.post('https://api.github.com/repos/dineshpabbi10/github-action-deals/dispatches', headers=headers, data=data)
+        return jsonify(
+            message="Action Sent"
+        )
+    else:
+        return jsonify(
+            message="Not Authenticated"
+        )
+
 
 if __name__ == '__main__':
    app.run(port=port_app)
