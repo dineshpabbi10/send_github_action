e168501e2de74df39fdc578e0f3c901e9331c8a2
Dinesh Pabbi <dineshpabbi10@gmail.com>
2021-10-17 14:02:27 -0700

    First commit

---
+++ Runner.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import jsonify
+import requests
+import os
+
+headers = {
+    'Accept': 'application/vnd.github+json',
+    'Authorization': 'token '+os.environ['GITHUB_TOKEN'],
+}
+
+data = '{"event_type": "fetch-deals"}'
+
+app = Flask(__name__)
+
+@app.route('/sendAction')
+def hello_world():
+    response_out = requests.post('https://api.github.com/repos/dineshpabbi10/github-action-deals/dispatches', headers=headers, data=data)
+    return jsonify(
+        message="Action Sent"
+    )
+
+if __name__ == '__main__':
+   app.run()
